e929fa0e6e5ff2eb1b81d83a8aeefa037853aa55
trynyen <tryn.yen@gmail.com>
2019-06-03 22:52:00 -0500

    second push

--- assets/game.js
+++ assets/game.js
@@ -10,29 +10,82 @@ var yellowCrystal;
 var greenCrystal;
 
 
-$(".number").on("click", function(reset){
-
+function random(){
     buttonRandom = Math.floor(Math.random()*100)+20;
-    $("#randomNumber").text(buttonRandom);
-
     redCrystal = Math.floor(Math.random()*10);  
-    $("#red").attr(redCrystal);
-
     blueCrystal = Math.floor(Math.random()*10); 
-    $("#blue").attr(blueCrystal);
-
     yellowCrystal = Math.floor(Math.random()*10); 
-    $("#yellow").attr(yellowCrystal);
-    
     greenCrystal = Math.floor(Math.random()*10); 
+}
+
+function reset(){
+    random();
+    totalScore = 0;
+    $("#score").text(totalScore)
+    $("#randomNumber").text(buttonRandom);
+    $("#red").attr(redCrystal);
+    $("#blue").attr(blueCrystal);
+    $("#yellow").attr(yellowCrystal);
     $("#green").attr(greenCrystal);
-})
+    $("#wins").text("Wins" + wins);
+    $("#losses").text("Losses" + losses);
+}
+
+function youWin(){
+    wins++;
+    $("#wins").text(wins);
+}
+
+function youLose(){
+    losses++;
+    $("#losses").text(losses);
+}
+
+reset();
 
 $(".crystal").on("click", function (){
+    
+    $("#score").text(redCrystal + blueCrystal + yellowCrystal + greenCrystal);
+    if ($("#score") < totalScore){
+        $("#score").text()
 
-})
-$("#score").text(redCrystal + blueCrystal + yellowCrystal + greenCrystal);
+        console.log(this);
+  if(!operatorPressed){
+  firstnum = firstnum + $(this).attr("value");
+  console.log(firstnum);
+  $("#first-number").text(firstnum);
+}
+var result = $("#result");
+$(".equal").on("click", function() {
+  var num1 = parseInt(firstnum);
+  var num2 = parseInt(secondnum);
+  if(operator==="plus"){
+    $("#result").text(num1 + num2);
+  } 
+  else if(operator==="minus"){
+    $("#result").text(num1 - num2);
+  }
 
+
+
+
+
+ else {
+  secondnum = secondnum + $(this).attr("value");
+  console.log(secondnum);
+  $("#second-number").text(secondnum);
+ }
+        
+    }
+    else if ($("#score") === totalScore){
+        youWin();
+        reset();
+    }
+    else {
+        youLose();
+        reset();
+    }
+    })
 });
 
 
